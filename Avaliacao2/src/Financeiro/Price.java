ac4ea0ccae2d7d2acdf7a873f6ecfa0beea7cbd8
Flavio Costa <skingk.f@gmail.com>
2018-05-18 17:15:20 -0300

    Questão 02 - Extraindo métodos objeto do método calcularPrestacoes()
    Signed-off-by: Flavio Costa <skingk.f@gmail.com>

--- Avaliacao2/src/Financeiro/Price.java
+++ Avaliacao2/src/Financeiro/Price.java
@@ -8,22 +8,43 @@ public class Price extends Modalidade {
 	public float calcularPrestacoes() {
 		parcelas = new Parcela[fin.getN()];
 		
-		float pmt = (float) (Math.pow((1+i), fin.getN()) * i);
-		
-		float pmtJuros = (float) (pmt/(Math.pow((1+i), fin.getN()) - 1));
-		
-		float pmtPV = fin.getPv() * pmtJuros;
-		
-		float pmtAmort = pmtPV - fin.getPv() * i;
+		float pmtBase = calcularBasePmt();
+		float pmtJuros = calcularJuros(pmtBase);
+		float pmtPV = calcularFinanciamentoPV(pmtJuros);
 		
 		for (int i=1; i<= fin.getN(); i++) {
-			float amortizacao = (float) (pmtAmort * Math.pow((1+this.i), i-1));
+			float amortizacao = calcularAmortizacao(pmtPV);
 			float juros = pmtPV - amortizacao;
 			Parcela p = new Parcela(pmtPV, juros, amortizacao);
 			parcelas[i-1] = p;
 		}
 		
 		return pmtPV;
+	}
+
+	public float calcularBasePmt() {
+		float pmtBase = (float) (Math.pow((1+i), fin.getN()) * i);
+		
+		return pmtBase;
+	}
+
+	public float calcularJuros(float pmtBase) {
+		float pmtJuros = (float) (pmtBase/(Math.pow((1+i), fin.getN()) - 1));
+		
+		return pmtJuros;
+	}
+	
+	public float calcularFinanciamentoPV(float pmtJuros) {
+		float pmtPV = fin.getPv() * pmtJuros;
+		
+		return pmtPV;
+	}
+	
+	public float calcularAmortizacao(float pmtPV){
+		float pmtAmort = pmtPV - fin.getPv() * i;
+		float amortizacao = (float) (pmtAmort * Math.pow((1+this.i), i-1));
+		
+		return amortizacao;
 	}
 	
 }
