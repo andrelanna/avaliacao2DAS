56e16ccec5df864244a7dc8f1193e41a9688b1fa
Marco Kuroiva <marco.antoniorl10@gmail.com>
2018-05-18 16:32:11 -0300

    Questao 1

--- src/Financeiro/Price.java
+++ src/Financeiro/Price.java
@@ -8,19 +8,22 @@ public class Price extends Modalidade {
 	public float calcularPrestacoes() {
 		parcelas = new Parcela[fin.getN()];
 		
-		float pmt = (float) (Math.pow((1+i), fin.getN()) * i);
-		pmt = (float) (pmt/(Math.pow((1+i), fin.getN()) - 1));
-		pmt = fin.getPv() * pmt;
+		// aplicando Dividir variável temporária em pmt:
 		
-		float a1 = pmt - fin.getPv() * i;
+		float pmt1, pmt2, pmt_final;
+		pmt1 = (float) (Math.pow((1+i), fin.getN()) * i);
+		pmt2 = (float) (pmt1/(Math.pow((1+i), fin.getN()) - 1));
+		pmt_final = fin.getPv() * pmt2;
+		
+		float a1 = pmt_final - fin.getPv() * i;
 		for (int i=1; i<= fin.getN(); i++) {
 			float amortizacao = (float) (a1 * Math.pow((1+this.i), i-1));
-			float juros = pmt - amortizacao;
-			Parcela p = new Parcela(pmt, juros, amortizacao);
+			float juros = pmt_final - amortizacao;
+			Parcela p = new Parcela(pmt_final, juros, amortizacao);
 			parcelas[i-1] = p;
 		}
 		
-		return pmt;
+		return pmt_final;
 	}
 	
 }
