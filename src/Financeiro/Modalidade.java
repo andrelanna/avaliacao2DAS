9f9e9fbeb950aab7bcf29f85ca4d5a6e6fc80d1e
lucasandrad <lucasandrad@yandex.com>
2018-05-18 17:41:09 -0300

    Finished question 3

--- src/Financeiro/Modalidade.java
+++ src/Financeiro/Modalidade.java
@@ -27,11 +27,22 @@ public abstract class Modalidade {
 		this.fin = fin;
 	}
 
+	/* 
+	De acordo com minha percepção a única relação indireta presente nesse método é 
+	a necessidade de chamar um método (getJuros();) da classe Parcela para que esse método 
+	retorne o valor da variável juros, que seguindo os princípios básicos de OO
+	é uma boa prática pois uma classe pública não deve oferecer acesso direto aos
+	seus atributos, e sim através getters e setters, assim o programador não intefere de 
+	maneira negativa na mutabilidade do atributo pois não consegue acessar o valor da 
+	variável diretamente.
+
+	Abaixo tem a minha resposta com o acesso direto à variável juros da classe Parcela.
+	*/
 	public float calcularTotalJuros() {
 		float juros = 0;
-		System.out.println(parcelas.length);
+		// System.out.println(parcelas.length);
 		for (Parcela p : parcelas) {
-			juros += p.getJuros();
+			juros += p.juros;
 		}
 		return juros;
 	}
