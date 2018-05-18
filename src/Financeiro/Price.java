8690fe22c8501fa177f34c07b82e6fc426968c1d
naiieandrade <andradenaiara@hotmail.com>
2018-05-18 17:50:55 -0300

    questao 3

--- src/Financeiro/Price.java
+++ src/Financeiro/Price.java
@@ -11,6 +11,7 @@ public class Price extends Modalidade {
 		final float pmt = (float) (Math.pow((1+i), fin.getN()) * i);
 		final float pmt2 = (float) (pmt/(Math.pow((1+i), fin.getN()) - 1));
 		final float valorParcela = fin.getPv() * pmt2;
+		System.out.println("pmt3 "+ valorParcela);
 		
 		float a1 = valorParcela - fin.getPv() * i;
 		for (int i=1; i<= fin.getN(); i++) {
@@ -18,8 +19,9 @@ public class Price extends Modalidade {
 			float juros = valorParcela - amortizacao;
 			Parcela p = new Parcela(valorParcela, juros, amortizacao);
 			parcelas[i-1] = p;
+			System.out.println("pmt3 "+valorParcela+ "p " + p.pmt);
 		}
-		
+		System.out.println("pmt3 "+ valorParcela);
 		return valorParcela;
 		
 	}
