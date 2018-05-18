5c2e3793f7e90f86c878139353f6159a9e2e806d
lugope <lucasgpereira.10@gmail.com>
2018-05-18 16:31:12 -0300

    Q1: Aplicando a operação de refatoração Dividir variável temporária

--- src/Financeiro/Price.java
+++ src/Financeiro/Price.java
@@ -8,9 +8,10 @@ public class Price extends Modalidade {
 	public float calcularPrestacoes() {
 		parcelas = new Parcela[fin.getN()];
 		
-		float pmt = (float) (Math.pow((1+i), fin.getN()) * i);
-		pmt = (float) (pmt/(Math.pow((1+i), fin.getN()) - 1));
-		pmt = fin.getPv() * pmt;
+		//Aplicando a operação de refatoração Dividir variável temporária
+		float pmt_pt1 = (float) (Math.pow((1+i), fin.getN()) * i);
+		float pmt_pt2 = (float) (pmt_pt1/(Math.pow((1+i), fin.getN()) - 1));
+		float pmt = fin.getPv() * pmt_pt2;
 		
 		float a1 = pmt - fin.getPv() * i;
 		for (int i=1; i<= fin.getN(); i++) {
