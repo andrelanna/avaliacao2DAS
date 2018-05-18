350567aa9eb6aad062b9fb0fcf1f736f2fac3c52
Leonardo Cambraia <cambraia@protonmail.com>
2018-05-18 17:28:08 -0300

    Questão 2

--- src/Financeiro/Price.java
+++ src/Financeiro/Price.java
@@ -1,30 +1,15 @@
 package Financeiro;
 
 public class Price extends Modalidade {
+
+	Calculadora calculadora = new Calculadora();
+
 	public Price(String banco, float i) {
 		super(banco, i);
 	}
 
 	public float calcularPrestacoes() {
-		parcelas = new Parcela[fin.getN()];
 
-		// float pmt = (float) (Math.pow((1+i), fin.getN()) * i);
-		// pmt = (float) (pmt/(Math.pow((1+i), fin.getN()) - 1));
-		// pmt = fin.getPv() * pmt;
-
-		float p_price1 = (float) (Math.pow((1+i), fin.getN()) * i);
-		float p_price2 = (float) (p_price1/(Math.pow((1+i), fin.getN()) - 1));
-		float pmt = fin.getPv() * p_price2;
-
-		float a1 = pmt - fin.getPv() * i;
-		for (int i=1; i<= fin.getN(); i++) {
-			float amortizacao = (float) (a1 * Math.pow((1+this.i), i-1));
-			float juros = pmt - amortizacao;
-			Parcela p = new Parcela(pmt, juros, amortizacao);
-			parcelas[i-1] = p;
-		}
-
-		return pmt;
+		return calculadora.calcularPrestacoes(this);
 	}
-
 }
