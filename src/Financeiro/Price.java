d65b785d190ffcf337e59fe3b24ae742fe688e14
João Paulo Alves Ferreira <iamferreirajp@gmail.com>
2018-05-18 16:34:30 -0300

    Questão 1 - Dividir variável temporária

--- src/Financeiro/Price.java
+++ src/Financeiro/Price.java
@@ -8,19 +8,19 @@ public class Price extends Modalidade {
 	public float calcularPrestacoes() {
 		parcelas = new Parcela[fin.getN()];
 		
-		float pmt = (float) (Math.pow((1+i), fin.getN()) * i);
-		pmt = (float) (pmt/(Math.pow((1+i), fin.getN()) - 1));
-		pmt = fin.getPv() * pmt;
+		final float pmt = (float) (Math.pow((1+i), fin.getN()) * i);
+		final float pmt2 = (float) (pmt/(Math.pow((1+i), fin.getN()) - 1));
+		final float pmt3 = fin.getPv() * pmt2;
 		
-		float a1 = pmt - fin.getPv() * i;
+		float a1 = pmt3 - fin.getPv() * i;
 		for (int i=1; i<= fin.getN(); i++) {
 			float amortizacao = (float) (a1 * Math.pow((1+this.i), i-1));
-			float juros = pmt - amortizacao;
-			Parcela p = new Parcela(pmt, juros, amortizacao);
+			float juros = pmt3 - amortizacao;
+			Parcela p = new Parcela(pmt3, juros, amortizacao);
 			parcelas[i-1] = p;
 		}
 		
-		return pmt;
+		return pmt3;
 	}
 	
 }
