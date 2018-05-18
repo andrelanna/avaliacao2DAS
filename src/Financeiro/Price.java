c5b76d2bef96c2127dd65248b4c633270e9a7646
Marcelo Augusto <mekmay@hotmail.com>
2018-05-18 17:25:01 -0300

    questão 2

--- src/Financeiro/Price.java
+++ src/Financeiro/Price.java
@@ -13,14 +13,14 @@ public class Price extends Modalidade {
 		final float temporariaTres = fin.getPv() * temporariaDois;
 		
 		final float a1 = temporariaTres - fin.getPv() * interest;
-		for (int i=1; i<= fin.getN(); i++) {
-			final float amortizacao = (float) (a1 * Math.pow((1+this.interest), i-1));
-			final float juros = temporariaTres - amortizacao;
-			Parcela p = new Parcela(temporariaTres, juros, amortizacao);
-			parcelas[i-1] = p;
-		}
 		
-		return temporariaTres;
+		CalcularPrestacoes calcularPrestacoes = new CalcularPrestacoes(this,
+				temporariaUm,
+				temporariaDois,
+				temporariaTres,
+				a1);
+		
+		return calcularPrestacoes.compute();
 	}
 	
 }
