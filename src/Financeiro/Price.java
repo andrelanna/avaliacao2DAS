1feef5d42482303e147494d1e9402087ed23b9ff
lugope <lucasgpereira.10@gmail.com>
2018-05-18 17:47:43 -0300

    Q2: Applicando refatoração extreir metodo objeto.

--- src/Financeiro/Price.java
+++ src/Financeiro/Price.java
@@ -1,17 +1,15 @@
 package Financeiro;
 
 public class Price extends Modalidade {
+	
 	public Price(String banco, float i) {
 		super(banco, i);
 	}
 
 	public float calcularPrestacoes() {
 		parcelas = new Parcela[fin.getN()];
-		
-		//Aplicando a operação de refatoração Dividir variável temporária
-		float pmt_pt1 = (float) (Math.pow((1+i), fin.getN()) * i);
-		float pmt_pt2 = (float) (pmt_pt1/(Math.pow((1+i), fin.getN()) - 1));
-		float pmt = fin.getPv() * pmt_pt2;
+		//Metodo objeto
+		float pmt = new Prestacao(this).calcular();
 		
 		float a1 = pmt - fin.getPv() * i;
 		for (int i=1; i<= fin.getN(); i++) {
