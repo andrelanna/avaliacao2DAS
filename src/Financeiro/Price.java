241ec94c01482b8803c6438564dfaa98d70bf1af
Vinicius Carvalho <vinisilvacar@gmail.com>
2018-05-18 17:04:28 -0300

    First Question

--- src/Financeiro/Price.java
+++ src/Financeiro/Price.java
@@ -7,20 +7,21 @@ public class Price extends Modalidade {
 
 	public float calcularPrestacoes() {
 		parcelas = new Parcela[fin.getN()];
+		float pmt;
 		
-		float pmt = (float) (Math.pow((1+i), fin.getN()) * i);
-		pmt = (float) (pmt/(Math.pow((1+i), fin.getN()) - 1));
-		pmt = fin.getPv() * pmt;
+		final float primaryPmt = (float) (Math.pow((1+i), fin.getN()) * i);
+		float secondPmt = (float) (primaryPmt/(Math.pow((1+i), fin.getN()) - 1));
+		float thirtPmt = fin.getPv() * secondPmt;
 		
-		float a1 = pmt - fin.getPv() * i;
+		float a1 = thirtPmt - fin.getPv() * i;
 		for (int i=1; i<= fin.getN(); i++) {
 			float amortizacao = (float) (a1 * Math.pow((1+this.i), i-1));
-			float juros = pmt - amortizacao;
-			Parcela p = new Parcela(pmt, juros, amortizacao);
+			float juros = thirtPmt - amortizacao;
+			Parcela p = new Parcela(thirtPmt, juros, amortizacao);
 			parcelas[i-1] = p;
 		}
 		
-		return pmt;
+		return thirtPmt;
 	}
 	
 }
