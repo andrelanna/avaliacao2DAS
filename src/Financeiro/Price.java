ff0ceed91ab5b795cdf0c24b4703f69d5002a94e
Lucas <lucasoliveira2306@gmail.com>
2018-05-18 17:33:58 -0300

    resolução da questão 1

--- src/Financeiro/Price.java
+++ src/Financeiro/Price.java
@@ -8,10 +8,10 @@ public class Price extends Modalidade {
 	public float calcularPrestacoes() {
 		parcelas = new Parcela[fin.getN()];
 		
-		float pmt = (float) (Math.pow((1+i), fin.getN()) * i);
-		pmt = (float) (pmt/(Math.pow((1+i), fin.getN()) - 1));
-		pmt = fin.getPv() * pmt;
-		
+	    final float numerador = (float) (Math.pow((1+i), fin.getN()) * i); 
+	    final float denominador = (float) (numerador/(Math.pow((1+i), fin.getN()) - 1)); 
+	    final float pmt = fin.getPv() * denominador; 
+	    
 		float a1 = pmt - fin.getPv() * i;
 		for (int i=1; i<= fin.getN(); i++) {
 			float amortizacao = (float) (a1 * Math.pow((1+this.i), i-1));
