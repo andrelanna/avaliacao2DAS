f51449d67dd439a110415544ee41dfe71ad49ea3
Ronyell Henrique <ronyellhenrique@gmail.com>
2018-05-18 20:51:05 +0000

    Questao 3

--- src/Financeiro/Financiamento.java
+++ src/Financeiro/Financiamento.java
@@ -51,4 +51,13 @@ public class Financiamento {
 	public int getN() {
 		return n;
 	}
+	
+	public float calcularTotalJuros(Modalidade modalidade) {
+		float juros = 0;
+		System.out.println(modalidade.parcelas.length);
+		for (Parcela p : modalidade.parcelas) {
+			juros += p.getJuros();
+		}
+		return juros;
+	}
 }
