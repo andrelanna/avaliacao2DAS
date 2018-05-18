de7f169d8f86fc2e4f4590d6319e1bfb48d7a7ee
Vitor Borges <vitorbertulucci@gmail.com>
2018-05-18 16:32:08 -0300

    [Divisão de variável temporária] Dividindo pmt em variáveis distintas - Aplicação de divisão de variável temporária transformando a variável pmt em pmt, dividedPmt e provision.

--- src/Financeiro/Price.java
+++ src/Financeiro/Price.java
@@ -8,19 +8,20 @@ public class Price extends Modalidade {
 	public float calcularPrestacoes() {
 		parcelas = new Parcela[fin.getN()];
 		
-		float pmt = (float) (Math.pow((1+i), fin.getN()) * i);
-		pmt = (float) (pmt/(Math.pow((1+i), fin.getN()) - 1));
-		pmt = fin.getPv() * pmt;
+		// Aplicação de dividir variável temporária. Transformação de pmt em pmt, dividedPmt e provision
+		final float pmt = (float) (Math.pow((1+i), fin.getN()) * i);
+		final float dividedPmt = (float) (pmt/(Math.pow((1+i), fin.getN()) - 1));
+		final float provision = fin.getPv() * dividedPmt;
 		
-		float a1 = pmt - fin.getPv() * i;
+		final float a1 = provision - fin.getPv() * i;
 		for (int i=1; i<= fin.getN(); i++) {
 			float amortizacao = (float) (a1 * Math.pow((1+this.i), i-1));
-			float juros = pmt - amortizacao;
-			Parcela p = new Parcela(pmt, juros, amortizacao);
+			float juros = provision - amortizacao;
+			Parcela p = new Parcela(provision, juros, amortizacao);
 			parcelas[i-1] = p;
 		}
 		
-		return pmt;
+		return provision;
 	}
 	
 }
